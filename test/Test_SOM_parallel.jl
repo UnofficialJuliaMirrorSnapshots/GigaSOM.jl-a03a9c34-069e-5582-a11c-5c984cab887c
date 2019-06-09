36214e2c4532feab9f35428fbbc97d4056f2196e
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-06-09 01:47:34 +0000

    Snapshot of branch develop taken on 2019-06-08T21:47:33.233-04:00 from "https://github.com/UnofficialJuliaMirror/GigaSOM.jl-a03a9c34-069e-5582-a11c-5c984cab887c"

--- test/Test_SOM_parallel.jl
+++ test/Test_SOM_parallel.jl
@@ -1,24 +1,28 @@
+using Test
+using Distributed
 
+p = addprocs(2)
+
+@everywhere using DistributedArrays
+@everywhere using GigaSOM
 
 # only use lineage_markers for clustering
 cc = map(Symbol, lineage_markers)
 df_som = daf.fcstable[:,cc]
-df_som_large = vcat(df_som,df_som)
-df_som_large = vcat(df_som_large, df_som)
-# topology is now always rectangular
-# som2 = initSOM_parallel(df_som, 10, 10)
+# df_som = vcat(df_som, df_som)
+
 som2 = initSOM_parallel(df_som, 10, 10)
 
-# using batch som with epochs
-# @time som2 = trainSOM_parallel(som2, df_som, size(df_som)[1], epochs = 10)
-som2 = trainSOM_parallel(som2, df_som_large, size(df_som_large)[1], epochs = 10)
+@time som2 = trainSOM_parallel(som2, df_som, size(df_som)[1], epochs = 10)
 
-@time mywinners = mapToSOM(som2, df_som)
+mywinners = mapToSOM(som2, df_som)
 CSV.write("cell_clustering_som.csv", mywinners)
 
 # myfreqs = SOM.classFrequencies(som2, daf.fcstable, :sample_id)
 
 codes = som2.codes
+@test size(codes) == (100,10)
+
 df_codes = DataFrame(codes)
 names!(df_codes, Symbol.(som2.colNames))
 CSV.write("df_codes.csv", df_codes)
